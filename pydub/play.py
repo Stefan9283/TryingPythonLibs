0d21745b4c10cece53ea4b4656592fa26bc1246f
Stefan9283 <stefan9283@gmail.com>
2020-08-31 06:28:23 +0300

    Auto-commit at Mon Aug 31 06:28:08 EEST 2020

--- pydub/play.py
+++ pydub/play.py
@@ -1,40 +1,29 @@
+#! /usr/bin/python3
 from pydub import AudioSegment
 from pydub.playback import play
+
+import pydub.effects as eff 
 
 song = AudioSegment.from_mp3("bruh.mp3")
 
 
-# boost volume by 6dB
-beginning = song + 6
+# boost volume by i dB
+result = song
 
-# reduce volume by 3dB
-end = song - 3
-#Concatenate audio (add one file to the end of another)
+for i in range(1, 3, 1):
+    new_part = song + i    
+    print(i)
+    #result = result + new_part
+    result.overlay(new_part)
 
-without_the_middle = beginning + end
-#How long is it?
+play(result)
 
-without_the_middle.duration_seconds == 15.0
-#AudioSegments are immutable
-
-# song is not modified
-backwards = song.reverse()
-#Crossfade (again, beginning and end are not modified)
-
-# 1.5 second crossfade
-with_style = beginning.append(end, crossfade=song.duration_seconds*1000-1)
-#Repeat
-
-# repeat the clip twice
-do_it_over = with_style * 2
-#Fade (note that you can chain operations because everything returns an AudioSegment)
-
-# 2 sec fade in, 3 sec fade out
-awesome = do_it_over.fade_in(3000).fade_out(4000)
-#Save the results (again whatever ffmpeg supports)
-
-awesome.export("mashup.mp3", format="mp3")
+result_rev=result.reverse()
+play(result_rev)
 
 
-mashup = AudioSegment.from_mp3("mashup.mp3")
-play(mashup)
+speed = eff.speedup(result, 1.5)
+
+play(speed)
+
+
