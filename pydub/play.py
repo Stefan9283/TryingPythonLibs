d659672856ceeab6a63fdf8e55be006c12e70594
Stefan9283 <stefan9283@gmail.com>
2020-08-31 07:31:17 +0300

    generators

--- pydub/play.py
+++ pydub/play.py
@@ -4,15 +4,14 @@ from pydub.playback import play
 
 import pydub.effects as eff 
 
-song = AudioSegment.from_mp3("bruh.mp3")
 
+song = AudioSegment.from_mp3("bruh.mp3")
 
 # boost volume by i dB
 result = song
 
 for i in range(1, 3, 1):
     new_part = song + i    
-    print(i)
     #result = result + new_part
     result.overlay(new_part)
 
@@ -25,5 +24,26 @@ play(result_rev)
 speed = eff.speedup(result, 1.5)
 
 play(speed)
+
+##############################################
+
+import pydub 
+
+from pydub import generators
+
+
+
+#pydub.utils.
+
+pulse_gen = generators.Pulse(523.25)
+
+sine_gen = generators.Sine(435)
+
+sine_sound = sine_gen.to_audio_segment()
+
+
+play(sine_sound)
+
+
 
 
