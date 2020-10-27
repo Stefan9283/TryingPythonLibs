25c8d719a9e82920c6ba7f3ae21c02bbc4635860
Stefan <stefan9283>
2020-10-28 01:06:16 +0200

    OpenCV

---
+++ OpenCV/README.md
@@ -0,0 +1,5 @@
+
+## Batch Edit Pics
+
+It reads all the files form the 'files' folder and adds the 
+greyscaled and blurred pics in the 'output' folder.
--- OpenCV/main.py
+++ OpenCV/main.py
@@ -1,15 +1,36 @@
 #! /usr/bin/python3
 import cv2
+import os
 
-# Read Image
-img = cv2.imread('testimg.jpg')
-# Display Image
-cv2.imshow('image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+dir = "files"
+outdir = 'output'
 
-# Applying Grayscale filter to image
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+if outdir not in os.listdir('.'):
+    os.makedirs(outdir + '/GRAY', 1)
+    os.makedirs(outdir + '/BLUR05', 1)
+    os.makedirs(outdir + '/BLUR10', 1)
 
-# Saving filtered image to new file
-cv2.imwrite('graytest.jpg', gray)
+for file in os.listdir(dir):
+    # print(file)
+    # Read Image
+    img = cv2.imread(dir + "/" + file)
+    # Display Image
+    # cv2.imshow('image', img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    out = "/" + os.path.splitext(file)[0]
+
+    # Applying Grayscale filter to image
+    grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Saving filtered image to new file
+    ksize = (5, 5)
+    blured5 = image = cv2.blur(img, ksize)
+    ksize = (10, 10)
+    blured10 = image = cv2.blur(img, ksize)
+
+    cv2.imwrite(outdir + '/GRAY'   + out + 'greytest.jpg', grey)
+    cv2.imwrite(outdir + '/BLUR05' + out + 'blur05test.jpg', blured5)
+    cv2.imwrite(outdir + '/BLUR10' + out + 'blur10test.jpg', blured10)
+
