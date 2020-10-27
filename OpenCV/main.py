cae15b0ebbc4131674ecff030f9b9f4fb7aa3147
Stefan <stefan9283>
2020-10-27 02:08:02 +0200

    OpenCV

---
+++ OpenCV/main.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+import cv2
+
+# Read Image
+img = cv2.imread('testimg.jpg')
+# Display Image
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Applying Grayscale filter to image
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Saving filtered image to new file
+cv2.imwrite('graytest.jpg', gray)
